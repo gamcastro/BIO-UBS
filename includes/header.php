cefc3cc27f3ee381892883e16e34187f1e190712
George Castro <gamcastro14@gmail.com>
2025-10-24 21:15:23 -0300

    Atualiza links e estrutura base da navbar para BS5

--- includes/header.php
+++ includes/header.php
@@ -11,91 +11,75 @@ require_once('includes/authorization.php');
   <title><?= $tituloDaPagina ?></title>
 
 
-  <script src="js/jquery.min.js"></script><!---------biblioteca java script jQuery---->
 
-  <script src="js/bootstrap.min.js"></script><!----biblioteca java script bootstrap (responsavel pela modal e outros efeitos)-->
 
-  <link rel="stylesheet" href="cssTema/bootstrap.min.css"><!-----biblioteca CSS bootstrap (responsável pelo designe da página)--->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script></script><!----biblioteca java script bootstrap (responsavel pela modal e outros efeitos)-->
 
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><!-----biblioteca CSS bootstrap (responsável pelo designe da página)--->
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"><!-----biblioteca de ícones bootstrap--->
+
+  <link rel="stylesheet" type="text/css" href="css/style.css"><!---arquivo de estilo personalizado-->
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 
 <body>
 
   <!-----------------Casca do Modal Novo Acolhimento----------------------------------->
-  <div id="acolhimentoBioUBS" class="modal fade" role="dialog">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-      </div>
+<div class="modal fade" id="acolhimentoBioUBS" tabindex="-1" aria-labelledby="acolhimentoModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-body text-center">
+         <div class="spinner-border" role="status">
+           <span class="visually-hidden">Carregando...</span>
+         </div>
+       </div>
     </div>
   </div>
+</div>
   <!-----------------Fim da Casca do Modal Novo Acolhimento------------------------------->
   
 
   <!------------------menu inicial para testes---- modelo Bootstrap------------------>
-
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">BioUBS</a>
-      </div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Home</a></li>
-
-        <li class="dropdown">
-          <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-            Cadastros <span class="caret"></span>
+<nav class="navbar navbar-expand-lg navbar-light bg-light"> <div class="container-fluid">
+    <a class="navbar-brand" href="#">BioUBS</a>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0"> <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cadastros
           </a>
-
-          <ul class="dropdown-menu">
-
-            <!-------CADASTRO DE PACIENTES--->
-            <li>
-              <a href="cadastroDePacientes.php" class="dropdown">
-                Pacientes
-              </a>
-            </li>
-
-
-
-            <!-------CADASTRO DE Profissionais --->
-            <li>
-              <a href="cadastroDeProfissionais.php" class="dropdown">
-                Profissionais
-              </a>
-            </li>
-            <!------------------------------->
-
-            <li>
-              <!-------CADASTRO DE UNIDADES --->
-              <a href="cadastroDeUnidades.php" class="dropdown">
-                Unidade
-              </a>
-            </li>
-
-
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="cadastroDePacientes.php">Pacientes</a></li>
+            <li><a class="dropdown-item" href="cadastroDeProfissionais.php">Profissionais</a></li>
+            <li><a class="dropdown-item" href="cadastroDeUnidades.php">Unidade</a></li>
           </ul>
-
         </li>
-
-        <li><a href="#">Documentos</a></li>
-        <li><a href="#">Relatórios</a></li>
-
-
-
-        <!-------NOVO ACOLHIMENTO --->
-
-        <li class="acolhimento-action">
-          <a href="modal/fluxos/modalAcolhimento.php"
-            data-toggle="modal"
-            data-target="#acolhimentoBioUBS">
-            <span class="glyphicon glyphicon-plus-sign"></span> Novo Acolhimento
-          </a>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Documentos</a>
         </li>
-        <!------------------------------->
+        <li class="nav-item">
+          <a class="nav-link" href="#">Relatórios</a>
+        </li>
       </ul>
-    </div>
-  </nav>
+      
+      <ul class="navbar-nav"> 
+          <li class="nav-item acolhimento-action"> <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#acolhimentoBioUBS">
+               <i class="bi bi-plus-circle-fill"></i> Novo Acolhimento
+             </a>
+          </li>
+      </ul>
+    </div> </div> </nav>
   <script src="js/custom.js"></script>
 
 
