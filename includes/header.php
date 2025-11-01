cf6ee5e059ffa90dae4e40545684c9d00dcfbeb4
George Castro <gamcastro14@gmail.com>
2025-11-01 19:01:55 -0300

    Atualizado header.php
    Retirado espaço antes do custom.css . No ambiente online estava desconfigurando o footer da página

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@ include_once( __DIR__ . '/authorization.php');
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.bootstrap5.css">
 
-  <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?> /css/custom.css"><!---arquivo de estilo personalizado-->
+  <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/css/custom.css"><!---arquivo de estilo personalizado-->
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
